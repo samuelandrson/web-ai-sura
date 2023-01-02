89d56795680969b0119042157edc900f0eefa436
samuelandrson <120358555+samuelandrson@users.noreply.github.com>
2023-01-02 06:41:03 -0700

    Update script.js

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://codex-im0y.onrender.com/', {
+    const response = await fetch('https://web-ai-sura-v60v.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
